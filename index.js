d443a92bb80f58362a3aa02252b15d6a351d6be0
Collin Edwards <collinedwards1579@gmail.com>
2018-01-22 20:12:05 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay(musicians, instruments){
+var array = []
+for(let i = 0; i < 4; i++){
+  array.push(`${musicians[i]} plays ${instruments[i]}`)
+}
+return array
+}
+
+function johnLennonFacts(facts){
+  let n = 0
+  while(n<4){
+    facts[n] + "!!!"
+    n++
+  }
+  return facts
+}
+
+function iLoveTheBeatles(n){
+var array = []
+  do {
+    n = array.push("I love the Beatles!")
+    n++
+  } while (n < 15)
+  return array
+}
